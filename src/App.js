ef77f5acb7769a0b399f46e3309502d823afa0ee
gatemadavid <dshefard@gmail.com>
2025-03-16 12:13:25 +0300

    initialize rproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import OverviewPage from './pages/OverviewPage';
+import ProductsPage from './pages/ProductsPage';
+
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+      <div className='fixed inset-0 z-0' >
+      <div className='absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80' />
+      </div>
+      <div className='absolute inset-0 backdrop-blur-sm' />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<OverviewPage />}/>
+        <Route path='/products' element={<ProductsPage />}/>
+      </Routes>
     </div>
   );
 }
